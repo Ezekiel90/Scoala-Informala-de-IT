e6a048b081f91e9de13e2417bad97836045f1f45
Grigoras <agrigoras@kpmg.com>
2022-07-26 11:19:13 +0300

    lista.py

--- Week 3/lista.py
+++ Week 3/lista.py
@@ -1,9 +1,39 @@
-lista = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
+list = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
 
-even_numbers = list(filter(lambda x: x%2 ==0, lista))
-uneven_numbers = list(filter(lambda x: x%2 != 0, lista))
-divisible_by_three = list(filter(lambda x: x%3 ==0, lista))
+def split_list(list):
+    odd_numbers = [number for number in list if number % 2 == 1]
+    even_numbers = [number for number in list if number % 2 == 0]
+    divisible_by_three = [number for number in list if number % 3 == 0]
+    return odd_numbers, even_numbers, divisible_by_three
+print("The odd numbers are {}".format(split_list(list)[0]))
+print("The even numbers are {}".format(split_list(list)[1]))
+print("The numbers divisible by three are {}".format(split_list(list)[2]))
 
-print(even_numbers)
-print(uneven_numbers)
-print(divisible_by_three)
+# even_numbers = list(filter(lambda x: x%2 ==0, lista))
+# uneven_numbers = list(filter(lambda x: x%2 != 0, lista))
+# divisible_by_three = list(filter(lambda x: x%3 ==0, lista))
+#
+# print(even_numbers)
+# print(uneven_numbers)
+# print(divisible_by_three)
+
+# for num in list
+#     if num % 2 == 0:
+#         print (num, end=" ")
+#
+#
+# for num in list:
+#     if num % 2 != 0:
+#         print (num, end=" ")
+
+
+# def get_even_numbers(list):
+#     even_numbers = []
+#
+#     for number in list:
+#         if number % 2 == 0:
+#             even_numbers.append(number)
+#
+#     return even_numbers
+#     list = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
+#     print(get_even_numbers(list))
--- Week 4/temafisiere.py
+++ Week 4/temafisiere.py
@@ -1,4 +1,5 @@
 import csv
+import datetime
 
 
 def introducere_categorii():
@@ -19,7 +20,20 @@ def introducere_taskurilor():
     while True:
         with open('taskuri.csv', 'a') as file:
             csv_writer = csv.writer(file, delimiter=',')
-            csv_writer.writerow([input('Adauga un task: ')])
+            task = input('Adauga un task: ')
+            data_limita = input('Adauga o data limita: ')
+            responsabil = input('Adauga persoana responsabila: ')
+            categoria = input('Adauga o categorie: ')
+            with open('categorii.txt', 'r') as file:
+                line = file.readlines()
+            verificare = categoria.strip()
+            new_list = [item.strip() for item in line]
+            if verificare not in new_list:
+                introducere_categ = input('Reintrodu categoria: ')
+                if introducere_categ in line:
+                    break
+                print('Categorie inexistenta ')
+            csv_writer.writerow([task, data_limita, responsabil, categoria])
         decizie = input('Doriti sa introduceti un alt task (Y/N)')
         print(decizie)
         if decizie.lower() == 'n':
